9f3f43887d2d90cf526f115a643105448b30ff7a
Hamzi-SE <hamzii.se@gmail.com>
2023-04-18 03:41:57 +0500

    fixed loader color in admin panel

--- skillsurge/src/components/Layout/Loader/Loader.jsx
+++ skillsurge/src/components/Layout/Loader/Loader.jsx
@@ -1,7 +1,15 @@
 import { Spinner, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Loader = ({ color = 'yellow.500' }) => {
+  const location = useLocation();
+
+  // if pathname includes /admin, then use the admin color
+  if (location.pathname.includes('/admin')) {
+    color = 'purple.500';
+  }
+
   return (
     <VStack h="100vh" justifyContent={'center'}>
       <div style={{ transform: 'scale(3)' }}>
