d3575f4fbe5f91474e4b99d78119c6d9b03c0b06
Muhammad Hamza <99138406+Hamzi-SE@users.noreply.github.com>
2023-08-04 07:06:48 +0500

    fix: send email

--- skillsurgeserver/utils/sendEmail.js
+++ skillsurgeserver/utils/sendEmail.js
@@ -4,6 +4,7 @@ export const sendEmail = async (to, subject, message) => {
 	const transporter = createTransport({
 		host: process.env.SMTP_HOST,
 		port: process.env.SMTP_PORT,
+		service: process.env.SMTP_SERVICE,
 		auth: {
 			user: process.env.SMTP_USER,
 			pass: process.env.SMTP_PASS,
