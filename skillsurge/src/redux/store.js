76a1c714e80bd6a793ab4d020e76d395106205b8
Muhammad Hamza <99138406+Hamzi-SE@users.noreply.github.com>
2023-08-10 01:38:37 +0500

    update: server link

--- skillsurge/src/redux/store.js
+++ skillsurge/src/redux/store.js
@@ -18,5 +18,4 @@ const store = configureStore({
 
 export default store;
 
-// export const server = 'https://skillsurgeserver.vercel.app/api/v1';
-export const server = 'http://localhost:4000/api/v1';
+export const server = 'https://skillsurgeserver.vercel.app/api/v1';
