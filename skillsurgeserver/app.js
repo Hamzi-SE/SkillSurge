46e2dc1c62ffe1b66d4c053fe74f441a249aef15
Hamzi-SE <hamzii.se@gmail.com>
2023-08-15 04:04:48 +0500

    add: access-control-allow-origin

--- skillsurgeserver/app.js
+++ skillsurgeserver/app.js
@@ -1,43 +1,44 @@
-import express from "express";
-import { config } from "dotenv";
-import ErrorMiddleware from "./middlewares/Error.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express'
+import { config } from 'dotenv'
+import ErrorMiddleware from './middlewares/Error.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 // dotenv config
-config({ path: "./config/config.env" });
+config({ path: './config/config.env' })
 
-const app = express();
+const app = express()
 
 // Using Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(
 	cors({
 		origin: process.env.FRONTEND_URL,
 		credentials: true, // mandatory
-		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+		allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 	})
-);
+)
 
 // Importing & Using Routes
-import courseRoutes from "./routes/courseRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import paymentRoutes from "./routes/paymentRoutes.js";
-import otherRoutes from "./routes/otherRoutes.js";
+import courseRoutes from './routes/courseRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import paymentRoutes from './routes/paymentRoutes.js'
+import otherRoutes from './routes/otherRoutes.js'
 
-app.use("/api/v1", courseRoutes);
-app.use("/api/v1", userRoutes);
-app.use("/api/v1", paymentRoutes);
-app.use("/api/v1", otherRoutes);
+app.use('/api/v1', courseRoutes)
+app.use('/api/v1', userRoutes)
+app.use('/api/v1', paymentRoutes)
+app.use('/api/v1', otherRoutes)
 
 // Default route
-app.get("/", (req, res) => {
-	res.send(`<h1>Welcome to SkillSurge Server. Frontend is live <a href=${process.env.FRONTEND_URL}>here</a></h1>`);
-});
+app.get('/', (req, res) => {
+	res.send(`<h1>Welcome to SkillSurge Server. Frontend is live <a href=${process.env.FRONTEND_URL}>here</a></h1>`)
+})
 
 // Error Handler (use at the end)
-app.use(ErrorMiddleware);
+app.use(ErrorMiddleware)
 
-export default app;
+export default app
